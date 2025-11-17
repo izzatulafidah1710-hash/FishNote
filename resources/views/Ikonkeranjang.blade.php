a0a458637120cae467594a58875c16d838f05b4c
Izzatul <izzatulafidah1710@gmail.com>
2025-11-17 11:07:16 +0700

    test

---
+++ resources/views/Ikonkeranjang.blade.php
@@ -0,0 +1,20 @@
+<li class="nav-item">
+    <a href="cart.php" class="nav-link position-relative">
+        <i class="fa fa-shopping-cart"></i>
+
+        <!-- Badge Cart -->
+        <span id="cart-count" 
+              style="
+                position: absolute;
+                top: -5px;
+                right: -10px;
+                background: red;
+                color: white;
+                font-size: 12px;
+                padding: 2px 6px;
+                border-radius: 50%;
+              ">
+            0
+        </span>
+    </a>
+</li>
---
+++ resources/views/slider.blade.php
@@ -0,0 +1,44 @@
+    <div id="slides-shop" class="cover-slides">
+        <ul class="slides-container">
+            <li class="text-center">
+                <img src="images/banner-01.jpg" alt="">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1 class="m-b-20"><strong>Welcome To <br> FishNote</strong></h1>
+                            <p class="m-b-40">Selamat datang di FishNote! Kami menyediakan ikan segar pilihan langsung dari sumbernya, berkualitas tinggi dan siap memenuhi kebutuhan dapur Anda. <br>
+                               Yuk, pilih ikan segar favorit Anda. Kesegaran terjamin, rasa lebih nikmat untuk hidangan keluarga.</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="text-center">
+                <img src="images/banner-02.jpg" alt="">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1 class="m-b-20"><strong>Welcome To <br> FishNote</strong></h1>
+                            <p class="m-b-40">Selamat datang di FishNote! Kami menyediakan ikan segar pilihan langsung dari sumbernya, berkualitas tinggi dan siap memenuhi kebutuhan dapur Anda. <br>
+                                Yuk, pilih ikan segar favorit Anda. Kesegaran terjamin, rasa lebih nikmat untuk hidangan keluarga.</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="text-center">
+                <img src="images/banner-03.jpg" alt="">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1 class="m-b-20"><strong>Welcome To <br> FishNote</strong></h1>
+                            <p class="m-b-40">Selamat datang di FishNote! Kami menyediakan ikan segar pilihan langsung dari sumbernya, berkualitas tinggi dan siap memenuhi kebutuhan dapur Anda. <br>
+                                Yuk, pilih ikan segar favorit Anda. Kesegaran terjamin, rasa lebih nikmat untuk hidangan keluarga.</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+        <div class="slides-navigation">
+            <a href="#" class="next"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+            <a href="#" class="prev"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
+        </div>
+    </div>
