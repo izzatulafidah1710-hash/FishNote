02f2c20b49b5d0805fffa5ad5382984eba8033f8
Izzatul <izzatulafidah1710@gmail.com>
2025-11-23 09:41:13 +0700

    perbaikan 1

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -12,7 +12,7 @@
                 <div class="col-md-6 col-lg-4 col-xl-4">
                     <div class="blog-box">
                         <div class="blog-img">
-                            <img class="img-fluid" src="images/blog-img.jpg" alt="" />
+                            <img class="img-fluid" src="images/dokumentasi bersama bang diva.jpg" alt="" />
                         </div>
                         <div class="blog-content">
                             <div class="title-blog">
@@ -32,31 +32,11 @@
                 <div class="col-md-6 col-lg-4 col-xl-4">
                     <div class="blog-box">
                         <div class="blog-img">
-                            <img class="img-fluid" src="images/blog-img-01.jpg" alt="" />
+                            <img class="img-fluid" src="images/dokumentasi bersama bang evan.jpg" alt="" />
                         </div>
                         <div class="blog-content">
                             <div class="title-blog">
                                 <h3>Dokumenrasi bersama pak Evan</h3>
-                                <p>Nulla ut urna egestas, porta libero id, suscipit orci. Quisque in lectus sit amet
-                                    urna dignissim feugiat. Mauris molestie egestas pharetra. Ut finibus cursus nunc sed
-                                    mollis. Praesent laoreet lacinia elit id lobortis.</p>
-                            </div>
-                            <ul class="option-blog">
-                                <li><a href="#"><i class="far fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fas fa-eye"></i></a></li>
-                                <li><a href="#"><i class="far fa-comments"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 col-xl-4">
-                    <div class="blog-box">
-                        <div class="blog-img">
-                            <img class="img-fluid" src="images/blog-img-02.jpg" alt="" />
-                        </div>
-                        <div class="blog-content">
-                            <div class="title-blog">
-                                <h3>Fusce in augue non nisi fringilla</h3>
                                 <p>Nulla ut urna egestas, porta libero id, suscipit orci. Quisque in lectus sit amet
                                     urna dignissim feugiat. Mauris molestie egestas pharetra. Ut finibus cursus nunc sed
                                     mollis. Praesent laoreet lacinia elit id lobortis.</p>
