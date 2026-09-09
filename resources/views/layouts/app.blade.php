6fcd68926602ef95bdc29368daa2c3c5e2f8d5ea
Aidil Ardiansyah <aidilardiansyahaidil9@gmail.com>
2026-09-09 18:01:18 +0700

    Fix CSS caching issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 
     <!-- Custom styles for this template-->
     <link href="{{ asset('template/css/sb-admin-2.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('template/css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('template/css/custom.css') }}?v={{ time() }}" rel="stylesheet">
 
     @stack('styles')
 
